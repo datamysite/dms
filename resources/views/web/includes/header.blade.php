08a010a699b855bfcfff18597128a964f61a5840
Waseem <waseem@datamysite.com>
2025-01-22 16:57:59 +0400

    og tags

--- resources/views/web/includes/header.blade.php
+++ resources/views/web/includes/header.blade.php
@@ -30,8 +30,8 @@
           </li>
           <li><a href="{{route('faq')}}" class="{{!empty($nav) && $nav == 'faq' ? 'active' : ''}}">FAQ</a></li>
           <!-- <li><a href="{{route('blogs')}}" class="{{!empty($nav) && $nav == 'blogs' ? 'active' : ''}}">Blogs</a></li> -->
-          <li><a href="{{route('about')}}" class="{{!empty($nav) && $nav == 'about' ? 'active' : ''}}">About Us</a></li>
           <li><a href="{{route('caseStudies')}}" class="{{!empty($nav) && $nav == 'case-studies' ? 'active' : ''}}">Case Studies</a></li>
+          <li><a href="{{route('about')}}" class="{{!empty($nav) && $nav == 'about' ? 'active' : ''}}">About Us</a></li>
           <li><a href="{{route('contact')}}" class="{{!empty($nav) && $nav == 'contact' ? 'active' : ''}}">Contact Us</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
