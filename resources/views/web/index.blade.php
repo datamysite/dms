ff61126b749593ed5f13c8a8d21875da981f0897
Waseem <waseem@datamysite.com>
2025-02-17 14:42:37 +0400

    og tags

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -22,7 +22,7 @@
 	          	<div class="hero-spacing"></div>
 	          	<h6 class="text-theme text-bold">D M S</h6>
 	            <h1>The New Level For Advertising</h1>
-	            <blockquote data-aos="fade-up" data-aos-delay="100">
+	            <blockquote>
 	              <p>People do not buy goods and services. <br>They buy relations, stories, and magic.</p>
 	            </blockquote>
 
