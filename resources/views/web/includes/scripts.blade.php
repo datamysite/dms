09fdb209e3d88b583f5013d97ad5f834b1326f4b
Waseem <waseem@datamysite.com>
2025-02-17 17:12:20 +0400

    og tags

--- resources/views/web/includes/scripts.blade.php
+++ resources/views/web/includes/scripts.blade.php
@@ -15,8 +15,8 @@
   <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.3.11/slick.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   
-  <script src="{{URL::to('/public')}}/assets/js/spinner.js"></script>
-  <script src="{{URL::to('/public')}}/assets/js/counter.js"></script>
-  <script src="{{URL::to('/public')}}/assets/js/faq.js"></script>
-  <script src="{{URL::to('/public')}}/assets/js/newsletter.js"></script>
-  <script src="{{URL::to('/public')}}/assets/js/enquiry.js"></script>
+  <script src="{{URL::to('/public')}}/assets/js/spinner.js" defer></script>
+  <script src="{{URL::to('/public')}}/assets/js/counter.js" defer></script>
+  <script src="{{URL::to('/public')}}/assets/js/faq.js" defer></script>
+  <script src="{{URL::to('/public')}}/assets/js/newsletter.js" defer></script>
+  <script src="{{URL::to('/public')}}/assets/js/enquiry.js" defer></script>
