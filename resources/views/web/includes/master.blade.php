d1cae6f3a6b55db8c3afcea413bb0069c7f3ff9a
Waseem <waseem@datamysite.com>
2025-05-29 12:20:27 +0400

    og tags

--- resources/views/web/includes/master.blade.php
+++ resources/views/web/includes/master.blade.php
@@ -60,7 +60,7 @@
     <img src="{{URL::to('/public/whatsapp-new.gif')}}" alt="Whatsapp Chat">
   </a>
 
-  <div class="modal fade" id="newsletter-modal-no" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal fade" id="newsletter-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
           <div class="modal-content">
               <div class="modal-header"> 
