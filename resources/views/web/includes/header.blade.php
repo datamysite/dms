40202cc24776fad763d2a5a0b51b14187f5451f1
Waseem <waseem@datamysite.com>
2025-09-09 18:15:39 +0400

    og tags

--- resources/views/web/includes/header.blade.php
+++ resources/views/web/includes/header.blade.php
@@ -2,7 +2,7 @@
     <div class="header-info">
       <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
         <span>
-          <a href="tel:971525287259" class="webCellPhone"><i class="bi bi-telephone"></i>&nbsp; +971 52 528 7259</a>
+          <a href="https://api.whatsapp.com/send/?phone=971525287259" class="webCellPhone"><i class="bi bi-telephone"></i>&nbsp; +971 52 528 7259</a>
         </span> 
 
         <span class="para">
