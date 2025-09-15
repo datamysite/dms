c10f20a5e7ac28c3330982626996edf5ef363c9f
Waseem <waseem@datamysite.com>
2025-09-15 20:16:14 +0400

    og tags

--- resources/views/web/includes/elements/clients.blade.php
+++ resources/views/web/includes/elements/clients.blade.php
@@ -11,7 +11,7 @@
 	      <div class="container">
 	      	<div class="row client-logos-block">
 	      		@for($i = 1; $i<=23; $i++)
-		            <div class="col-lg-1 client-logos">
+		            <div class="col-lg-1 col-sm-6 client-logos">
 		            	<img src="{{URL::to('/public/assets/img/clients/'.$i.'.png')}}" class="" height="45px" alt="Clients Logo {{$i}}" loading="lazy">
 		            </div><!-- End testimonial item -->
 	            @endfor
