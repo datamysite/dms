a2462ad864c4a0be8b5c53139109401d8171d54e
Waseem <waseem@datamysite.com>
2025-10-06 16:42:26 +0400

    og tags

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -49,7 +49,7 @@ class ViewServiceProvider extends ServiceProvider
                                                 })->get();
 
             $data['header_services'] = Services::where('parent_id', '0')->orderBy('navOrder')->get();
-            $data['blog_categories'] = Categories::has('blogs', '>', 0)->withCount('blogs')->orderBy('name')->get();
+            $data['blog_categories'] = Categories::withCount('blogs')->having('blogs_count', '>', 0)->orderBy('name')->get();
             
             $view->with($data);
         });
