7b91fcdce540a83f4b417eeed9f53990d705e82d
Waseem <waseem@datamysite.com>
2025-10-06 13:02:27 +0400

    og tags

--- resources/views/web/includes/scripts.blade.php
+++ resources/views/web/includes/scripts.blade.php
@@ -1,8 +1,8 @@
 
   <!-- Vendor JS Files -->
-  <script src="{{URL::to('/public')}}/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="{{URL::to('/public')}}/assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="{{URL::to('/public')}}/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="{{URL::to('/public')}}/assets/vendor/bootstrap/js/bootstrap.bundle.min.js" defer></script>
+  <script src="{{URL::to('/public')}}/assets/vendor/glightbox/js/glightbox.min.js" defer></script>
+  <script src="{{URL::to('/public')}}/assets/vendor/swiper/swiper-bundle.min.js" defer></script>
 
   <!-- Main JS File -->
   <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
@@ -14,7 +14,7 @@
   <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/5.0.9/jquery.inputmask.min.js"></script> -->
   <script src="{{URL::to('/public')}}/assets/js/main.js"></script>
  <!--  <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.3.11/slick.min.js"></script> -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
   
   <!-- <script src="{{URL::to('/public')}}/assets/js/spinner.js" defer></script> -->
 <!--   <script src="{{URL::to('/public')}}/assets/js/counter.js" defer></script> -->
