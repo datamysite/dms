749c0299554ed44105e628182f5794faafdb5658
Waseem <waseem@datamysite.com>
2025-10-06 13:50:06 +0400

    og tags

--- resources/views/web/includes/elements/case-studies.blade.php
+++ resources/views/web/includes/elements/case-studies.blade.php
@@ -35,25 +35,25 @@
 
                 <div class="col-lg-3 col-sm-6">
                     <a href="{{URL::to('/public/case-studies/dubai-duty-free.pdf')}}" target="_blank" class="open-case-study">
-                        <img src="{{URL::to('/public/case-studies/dubai-duty-free.jpg')}}" class="case-studies-img" width="100%" alt="Case Studies">
+                        <img src="{{URL::to('/public/case-studies/cover/dubai-duty-free.jpg')}}" class="case-studies-img" width="100%" alt="Case Studies">
                         <h5><span>Dubai Duty Free</span> x <span>DMS</span> |  Case Study</h5>
                     </a>
                 </div>
                 <div class="col-lg-3 col-sm-6">
                     <a href="{{URL::to('/public/case-studies/lenskart.pdf')}}" target="_blank" class="open-case-study">
-                        <img src="{{URL::to('/public/case-studies/lenskart.jpg')}}" class="case-studies-img-even" width="100%" alt="Case Studies">
+                        <img src="{{URL::to('/public/case-studies/cover/lenskart.jpg')}}" class="case-studies-img-even" width="100%" alt="Case Studies">
                         <h5><span>Lenskart</span> x <span>DMS</span> |  Case Study</h5>
                     </a>
                 </div>
                 <div class="col-lg-3 col-sm-6">
                     <a href="{{URL::to('/public/case-studies/danube.pdf')}}" target="_blank" class="open-case-study">
-                        <img src="{{URL::to('/public/case-studies/danube.jpg')}}" class="case-studies-img" width="100%" alt="Case Studies">
+                        <img src="{{URL::to('/public/case-studies/cover/danube.jpg')}}" class="case-studies-img" width="100%" alt="Case Studies">
                         <h5><span>Danube</span> x <span>DMS</span> |  Case Study</h5>
                     </a>
                 </div>
                 <div class="col-lg-3 col-sm-6">
                     <a href="{{URL::to('/public/case-studies/ksa.pdf')}}" target="_blank" class="open-case-study">
-                        <img src="{{URL::to('/public/case-studies/ksa.jpg')}}" class="case-studies-img-even" width="100%" alt="Case Studies">
+                        <img src="{{URL::to('/public/case-studies/cover/lenskart.jpg')}}" class="case-studies-img-even" width="100%" alt="Case Studies">
                         <h5><span>KSA Influencers</span> x <span>DMS</span> |  Case Study</h5>
                     </a>
                 </div>
--- resources/views/web/includes/elements/clients.blade.php
+++ resources/views/web/includes/elements/clients.blade.php
@@ -10,18 +10,18 @@
                 <br><br>
                 <div class="slider mts2">
                     @for($i = 1; $i<=11; $i++)
-                        <div class="item"><a href=""><img alt="logo" src="{{URL::to('/public/assets/img/clients-final/'.$i.'.png')}}"></a></div>
+                        <div class="item"><a href=""><img alt="logo" src="{{URL::to('/public/assets/img/clients-final/'.$i.'.png')}}" loading="lazy" alt="Client Logo"></a></div>
                     @endfor
                     @for($i = 1; $i<=11; $i++)
-                        <div class="item"><a href=""><img alt="logo" src="{{URL::to('/public/assets/img/clients-final/'.$i.'.png')}}"></a></div>
+                        <div class="item"><a href=""><img alt="logo" src="{{URL::to('/public/assets/img/clients-final/'.$i.'.png')}}" loading="lazy" alt="Client Logo"></a></div>
                     @endfor
                 </div>
                 <div class="slider mts">
                     @for($i = 12; $i<=23; $i++)
-                        <div class="item"><a href=""><img alt="logo" src="{{URL::to('/public/assets/img/clients-final/'.$i.'.png')}}"></a></div>
+                        <div class="item"><a href=""><img alt="logo" src="{{URL::to('/public/assets/img/clients-final/'.$i.'.png')}}" loading="lazy" alt="Client Logo"></a></div>
                     @endfor
                     @for($i = 12; $i<=23; $i++)
-                        <div class="item"><a href=""><img alt="logo" src="{{URL::to('/public/assets/img/clients-final/'.$i.'.png')}}"></a></div>
+                        <div class="item"><a href=""><img alt="logo" src="{{URL::to('/public/assets/img/clients-final/'.$i.'.png')}}" loading="lazy" alt="Client Logo"></a></div>
                     @endfor
                 </div>
             </div>
