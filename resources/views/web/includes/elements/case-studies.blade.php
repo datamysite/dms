bc6c650dfe9b427570933a74c90e7ed428bd982e
Waseem <waseem@datamysite.com>
2026-04-22 18:04:32 +0400

    og tags

--- resources/views/web/includes/elements/case-studies.blade.php
+++ resources/views/web/includes/elements/case-studies.blade.php
@@ -75,7 +75,7 @@
                     </div>
                     <div class="swiper-slide">
                         <a href="{{URL::to('/public/case-studies/ksa.pdf')}}" target="_blank" class="open-case-study">
-                            <img src="{{URL::to('/public/case-studies/cover/lenskart.jpg')}}" class="case-studies-img-even" width="100%" alt="Case Studies">
+                            <img src="{{URL::to('/public/case-studies/cover/lenskart-p.png')}}" class="case-studies-img-even" width="100%" alt="Case Studies">
                             <h5><span>KSA Influencers</span> x <span>DMS</span> |  Case Study</h5>
                         </a>
                     </div>
